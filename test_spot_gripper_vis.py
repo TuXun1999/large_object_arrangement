07016fe2e65c487948594e35744bbe04a8941650
TuXun1999 <617612390@qq.com>
2025-05-14 18:50:14 +0000

    First test on teleoperation system

--- test_spot_gripper_vis.py
+++ test_spot_gripper_vis.py
@@ -9,37 +9,34 @@ f = open("./gripper_poses_move_chair.json", "r")
 gripper_poses_trials = json.load(f)
 
 gripper_poses_all = gripper_poses_trials
-gripper_poses_sample = np.load(open("./gripper_pose_sample.npy", 'rb'))
+
 # Create the window to display everything
 vis= o3d.visualization.Visualizer()
 vis.create_window()
 
-color_scale = 1/(len(gripper_poses_all) - 1)
+print(len(gripper_poses_all))
 for i in range(len(gripper_poses_all)):
     gripper_poses = gripper_poses_all[i]
     for item in gripper_poses:
+        item = np.array(item)
+        camera_pose = item[:, 0:4]
+        body_pose = item[:, 4:]
+        camera_pose = body_pose@camera_pose # Fit the transformation also
         camera_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-        camera_frame.scale(0.01, [0, 0, 0])
-        gripper_pose = item
-        camera_frame.transform(gripper_pose)
-        #camera_frame.paint_uniform_color((1 - i * color_scale, 0, i * color_scale))
+        camera_frame.scale(0.1, [0, 0, 0])
+        body_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+        body_frame.scale(0.1, [0, 0, 0])
+        camera_frame.transform(camera_pose)
+        body_frame.transform(body_pose)
+        camera_frame.paint_uniform_color((1, 0, 0))
+        body_frame.paint_uniform_color((0, 1, 0))
         vis.add_geometry(camera_frame)
+        vis.add_geometry(body_frame)
 
-for pose in gripper_poses_sample:
-    camera_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-    camera_frame.scale(0.01, [0, 0, 0])
-    gripper_pose = pose
-    camera_frame.transform(gripper_pose)
-    # camera_frame.paint_uniform_color((0, 1, 0))
-    vis.add_geometry(camera_frame)
 
-fundamental_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
-fundamental_frame.scale(0.1, [0, 0, 0])
-vis.add_geometry(fundamental_frame)
-print("===Test==")
-print(gripper_poses_sample[0])
-print(gripper_poses_sample[30])
-print(np.linalg.inv(gripper_poses_sample[0])@gripper_poses_sample[30])
+# fundamental_frame = o3d.geometry.TriangleMesh.create_coordinate_frame()
+# fundamental_frame.scale(0.1, [0, 0, 0])
+# vis.add_geometry(fundamental_frame)
 vis.run()
 # Close all windows
 vis.destroy_window()
